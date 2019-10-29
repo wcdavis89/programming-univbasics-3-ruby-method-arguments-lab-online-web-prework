b51d3e81cedac4f7294cc64ed9f1f96943fefed5
Whittney Davis <wcdavis89@yahoo.com>
2019-10-29 18:52:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ end
 name="Dan"
 language="Ember.js"
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional (name, language="Ruby")
